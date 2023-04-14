a865713d1a642719b877b3b9076d39446c7d3dd7
mathew <mtalagtag@gbox.adnu.edu.ph>
2023-04-15 00:02:00 +0800

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
  <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [ProductController::class, 'index'])->name('shop');
